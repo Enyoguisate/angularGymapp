bef295c640d640135c1935550e78322123fd50d7
vmartin86 <victormartin9086@gmail.com>
2018-11-19 18:50:16 -0300

    added user functionality

--- src/app/pages/user/user.component.ts
+++ src/app/pages/user/user.component.ts
@@ -1,8 +1,9 @@
 import { Component, OnInit } from '@angular/core';
-import { ACTIVITY_DATA } from '../../_helper/trainer-data';
-import { Activity, Activities } from "../../interfaces/index";
+import { ACTIVITY_DATA, SHIFT_DATA } from '../../_helper/trainer-data';
+import { Activity, Activities, Shift } from "../../interfaces/index";
+import * as moment from "moment";
 
-@Component({
+@Component({  
   selector: 'app-user',
   templateUrl: './user.component.html',
   styleUrls: ['./user.component.css']
@@ -12,10 +13,13 @@ export class UserComponent implements OnInit {
   public activityData: Activity[] = ACTIVITY_DATA;
   public activitiesToShow: Activity[] = [];
   public activityDateSelected: string = '';
-  public activitiesAvailable: Activities[] = [];
+  public activitiesAvailable: Shift[] = [];
   public selectedActivity: Activity = {};
-
-
+  public showScheduledClasses: boolean = false;
+  public shiftData: Shift[] = SHIFT_DATA;
+  private dateSelected: string = '';
+  private activitySelected: Activity = {};
+  
   constructor(
 
   ) { 
@@ -24,13 +28,37 @@ export class UserComponent implements OnInit {
 
   ngOnInit() {
   }
-
-  dateChange($event){
-    console.log('$event', $event);
-  }
-
+  
   onActivityChange($event){
-    console.log('$event', $event);
+    // console.log('$event', $event);
+    // console.log('selectedActivity', this.selectedActivity);
+    // this.activitySelected = this.activi
+    this.clearOptions();
   }
+
+  onDateChange($event){
+    this.clearOptions();
+    // console.log('$event', $event);
+    // console.log('activityDateSelected', this.activityDateSelected);
+    this.dateSelected = moment(this.activityDateSelected).format("DD-MM-YY").toString();
+    this.shiftData.forEach((shift: Shift) => 
+    {
+      if(shift.idActivity === +this.selectedActivity && this.dateSelected.endsWith(shift.date) && shift.quotaLeft > 0){
+        this.activitiesAvailable.push(shift);
+      }
+    });
+  
+    this.showScheduledClasses = true;
+  }
+
+  selectSchedule(shift){
+    // console.log('shift Selected', shift);
+  }
+
+  clearOptions(){
+    this.activitiesAvailable = [];
+    this.showScheduledClasses = false;
+  }
+
 
 }
